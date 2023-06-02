1a6010c7e49c73bc51d720eaa316947da2ceaaa2
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-06-02 12:10:16 -0400

    Implementing tests on DataManager.attemptLogin method

--- org/DataManager_attemptLogin_Test.java
+++ org/DataManager_attemptLogin_Test.java
@@ -17,8 +17,27 @@ public class DataManager_attemptLogin_Test {
 
     @Test
     public void testSuccessfulLogin() {
-        DataManager dm = new DataManager(new WebClient("localhost", 3001));
+        DataManager dm = new DataManager(new WebClient("localhost", 3001) {
+            @Override
+            public String makeRequest(String resource, Map<String, Object> queryParams) {
+                return "{\"status\":\"success\",\"data\":{\"_id\":\"123\",\"login\":\"Patrick\",\"password\":\"1020\"," +
+                        "\"name\":\"Patrick\",\"description\":\"KC\",\"funds\":[" +
+                        "{\"target\":5000," +
+                        "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
+                        "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[{\"_id\":\"647a134ed830f6d7d4353a84\",\"contributor\":\"6479e484e99150cda7f6dd78\",\"fund\":\"6479e450e99150cda7f6dd6f\",\"date\":\"2023-06-02T16:05:34.735Z\",\"amount\":100,\"__v\":0}],\"__v\":0}," +
+                        "{\"target\":5000," +
+                        "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
+                        "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[],\"__v\":0}" + "],\"__v\":0}}";
+            };
+
+        });
         Organization org = dm.attemptLogin("Patrick", "1020");
+        assertNotNull(org);
+        assertEquals("Patrick", org.getName());
+        assertEquals("123", org.getId());
+        assertEquals("KC", org.getDescription());
+        assertEquals(2, org.getFunds().size());
+
     }
 }
 
