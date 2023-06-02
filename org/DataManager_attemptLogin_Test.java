0bc58382c236fee01d3dc687f59c6c4a216eb63c
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-06-02 11:01:13 -0400

    Add database data dir

--- org/DataManager_attemptLogin_Test.java
+++ org/DataManager_attemptLogin_Test.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DataManager_attemptLogin_Test {
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import javax.xml.crypto.Data;
+
+public class DataManager_attemptLogin_Test {
+    /*
+     * This is a test class for the DataManager.attemptLogin method.
+     * Add more tests here for this method as needed.
+     *
+     * When writing tests for other methods, be sure to put them into separate
+     * JUnit test classes.
+     */
+
+    @Test
+    public void testSuccessfulLogin() {
+        DataManager dm = new DataManager(new WebClient("localhost", 3001));
+        Organization org = dm.attemptLogin("Patrick", "1020");
+    }
 }
+
