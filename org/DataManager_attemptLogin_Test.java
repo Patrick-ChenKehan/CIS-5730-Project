1a76e20f3a07b6af8caf9a38551cc56419d440b4
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2023-06-13 02:33:21 -0700

    Update DataManager_attemptLogin_Test.java

--- org/DataManager_attemptLogin_Test.java
+++ org/DataManager_attemptLogin_Test.java
@@ -22,13 +22,13 @@ public class DataManager_attemptLogin_Test {
             public String makeRequest(String resource, Map<String, Object> queryParams) {
                 if (resource == "/findOrgByLoginAndPassword")
                     return "{\"status\":\"success\",\"data\":{\"_id\":\"123\",\"login\":\"Patrick\",\"password\":\"1020\"," +
-                        "\"name\":\"Patrick\",\"description\":\"KC\",\"funds\":[" +
-                        "{\"target\":5000," +
-                        "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
-                        "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[{\"_id\":\"647a134ed830f6d7d4353a84\",\"contributor\":\"6479e484e99150cda7f6dd78\",\"fund\":\"6479e450e99150cda7f6dd6f\",\"date\":\"2023-06-02T16:05:34.735Z\",\"amount\":100,\"__v\":0}],\"__v\":0}," +
-                        "{\"target\":5000," +
-                        "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
-                        "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[],\"__v\":0}" + "],\"__v\":0}}";
+                            "\"name\":\"Patrick\",\"description\":\"KC\",\"funds\":[" +
+                            "{\"target\":5000," +
+                            "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
+                            "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[{\"_id\":\"647a134ed830f6d7d4353a84\",\"contributor\":\"6479e484e99150cda7f6dd78\",\"fund\":\"6479e450e99150cda7f6dd6f\",\"date\":\"2023-06-02T16:05:34.735Z\",\"amount\":100,\"__v\":0}],\"__v\":0}," +
+                            "{\"target\":5000," +
+                            "\"_id\":\"6479e450e99150cda7f6dd6f\",\"name\":\"Philan\",\"description\":\"Children Fund\"," +
+                            "\"org\":\"6479e41ce99150cda7f6dd6c\",\"donations\":[],\"__v\":0}" + "],\"__v\":0}}";
                 else if (resource == "/findContributorNameById")
                     return "{\"status\":\"success\",\"data\":\"wj\"}";
                 return null;
@@ -44,6 +44,7 @@ public class DataManager_attemptLogin_Test {
 
     }
 
+    // similar to test in Robustness Test
     @Test(expected=IllegalStateException.class)
     public void testFailedLogin() {
         DataManager dm = new DataManager(new WebClient("localhost", 3001) {
@@ -56,6 +57,7 @@ public class DataManager_attemptLogin_Test {
         Organization org = dm.attemptLogin("Patrick", "1020f");
     }
 
+    // similar to test in Robustness Test
     @Test (expected = IllegalStateException.class)
     public void testFailedConnection() {
         DataManager dm = new DataManager(new WebClient("localhost", 3001) {
