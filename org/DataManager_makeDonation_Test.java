a1f10ff8c89ed9c1b2591ca748e953a91b1f848e
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2023-06-25 21:27:21 -0700

    Update DataManager_makeDonation_Test.java

--- org/DataManager_makeDonation_Test.java
+++ org/DataManager_makeDonation_Test.java
@@ -16,18 +16,24 @@ public class DataManager_makeDonation_Test {
 
             @Override
             public String makeRequest(String resource, Map<String, Object> queryParams) {
-                return "{\"status\":\"success\"}";
+                if (resource.equals("/makeDonation")) {
+                    return "{\"status\":\"success\",\"data\":{\"_id\":\"64990e2d1202ad4a87f3a86f\"," +
+                            "\"contributor\":\"649873aaf59da441a42c1de1\",\"fund\":\"64990c3a1202ad4a87f3a851\"" +
+                            ",\"date\":\"2023-06-26T04:03:57.997Z\",\"amount\":40,\"__v\":0}}";
+                } else if (resource.equals("/findContributorNameById")){
+                    return "{\"status\":\"success\",\"data\":\"wj\"}";
+                }
+                return null;
             }
 
         });
 
-
-        Donation donation = dm.makeDonation("649873aaf59da441a42c1de1", "64988c3cd99e3741cd73d527", 70);
+        Donation donation = dm.makeDonation("649873aaf59da441a42c1de1", "64990c3a1202ad4a87f3a85", 40);
         assertNotNull(donation);
-        assertEquals("", donation.getAmount());
-        assertEquals("", donation.getContributorName());
-        assertEquals("", donation.getFundId());
-        assertEquals(10000, donation.getDate());
+        assertEquals(40, donation.getAmount());
+        assertEquals("wj", donation.getContributorName());
+       // assertEquals("", donation.getFundId());
+        assertEquals("2023-06-26T04:03:57.997Z", donation.getDate());
     }
 
 }
